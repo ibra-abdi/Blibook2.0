977322956f9e397c07ac0e2cf8a8d6e1f0a18859
Naufalar10 <naufalariq1919@gmail.com>
2023-06-19 00:31:47 +0700

    Admin and User Authentication

---
+++ resources/views/livewire/user/user-dashboard-component.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>usher Dashboard</h1>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Livewire\Admin\AdminDashboardComponent;
+use App\Http\Livewire\User\UserDashboardComponent;
+use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CheckoutComponent;
 use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\ShopComponent;
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
@@ -21,11 +24,28 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/', HomeComponent::class)->name('home.index');
-
 Route::get('/shop', ShopComponent::class)->name('shop');
-
 Route::get('/cart', CartComponent::class)->name('shop.cart');
+Route::get('/checkout', CheckoutComponent::class)->name('checkout.cart');
 
-Route::get('/checkout', CheckoutComponent::class)->name('shop.checkout');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/user/dashboard', UserDashboardComponent::class)->name('user.dashboard');
+});
+
+Route::middleware(['auth', 'authadmin'])->group(function () {
+    Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
+});
+
+require __DIR__ . '/auth.php';
