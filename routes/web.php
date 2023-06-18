93fbe042d3800f78f10b17cda43b9032f7d99ca5
Naufalar10 <naufalariq1919@gmail.com>
2023-06-18 23:27:38 +0700

    setup and Create Shop, Cart And Checkout Page

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ShopComponent;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/', HomeComponent::class)->name('home.index');
+
+Route::get('/shop', ShopComponent::class)->name('shop');
+
+Route::get('/cart', CartComponent::class)->name('shop.cart');
+
+Route::get('/checkout', CheckoutComponent::class)->name('shop.checkout');
