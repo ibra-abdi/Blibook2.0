0c76fd057e72301290780ed8cb3c569226b8b1a3
ibra-abdi <itjustworks1@gmail.com>
2023-06-19 06:55:19 +0700

    Categorize Product

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Livewire\DetailsComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\CategoryComponent;
 use App\Http\Livewire\CheckoutComponent;
 use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\ShopComponent;
@@ -34,6 +35,9 @@ Route::get('/shop', ShopComponent::class)->name('shop');
 Route::get('/product/{slug}', DetailsComponent::class)->name('product.details');
 Route::get('/cart', CartComponent::class)->name('shop.cart');
 Route::get('/checkout', CheckoutComponent::class)->name('checkout.cart');
+Route::get('/product-category/{slug}', CategoryComponent::class)->name('product.category');
+
+// Import
 
 
 Route::middleware('auth')->group(function () {
